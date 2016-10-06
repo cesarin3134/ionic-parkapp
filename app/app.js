949f3ffdfaf9e4ad4e39c484a9665cc7e488594a
Cesar Andavisa <cesar.andavisa@gmail.com>
2016-10-06 17:48:27 +0200

    added calendar modal and item list page

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@
 angular.module('carpark', [
   // load your modules here
   'main', // starting with the main module
-]).config(['$ionicConfigProvider', function ($ionicConfigProvider) {
-  $ionicConfigProvider.navBar.alignTitle('center');
-  $ionicConfigProvider.views.transition('none');
+]).config(['$ionicConfigProvider', function () {
+  //$ionicConfigProvider.navBar.alignTitle('center');
+  //$ionicConfigProvider.views.transition('none');
 }]);
--- app/main/controllers/carParkList-ctrl.js
+++ app/main/controllers/carParkList-ctrl.js
@@ -4,9 +4,25 @@
 'use strict';
 
 (function (angular) {
-  angular.module('main').controller('carParkListCtrl', ['$log', '$scope', 'parkListSrv',
-    function ($log, $scope, parkListSrv) {
+  angular.module('main').controller('carParkListCtrl', ['$log', '$scope', '$ionicModal', 'parkListSrv',
+    function ($log, $scope, $ionicModal, parkListSrv) {
+
+      var mv = this;
+      mv.showCalendar = _showCalendar;
       $scope.parkingList = parkListSrv.getParkingList();
+
+
+
+      function _showCalendar () {
+
+        $ionicModal.fromTemplateUrl('main/templates/calendar-modal.html', {
+          scope: $scope
+        }).then(function(modal) {
+          $scope.calendarModal = modal;
+          $scope.calendarModal.show();
+
+        });
+      }
     }]);
 
 })(angular);
