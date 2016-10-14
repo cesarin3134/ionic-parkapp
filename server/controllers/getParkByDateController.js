5bd79d6d95895577d85bc3f9f064d4c75d754efd
Cesar Andavisa <cesar.andavisa@gmail.com>
2016-10-14 16:16:17 +0200

    filter by date

--- server/controllers/getParkByDateController.js
+++ server/controllers/getParkByDateController.js
@@ -2,12 +2,26 @@ module.exports = function (app, route) {
   var Park = app.models.park;
   return {
     "handler": function (req, res) {
-      if (req.params.id) {
-        var employeeCode = req.params.id.toUpperCase();
-        Park.find({"allocations.employeeCode": employeeCode}, function (error, park) {
+
+      if (req.params.date) {
+
+        var parseDate = parseInt(req.params.date);
+        /*mongoDb save format date as a ISODate(2016-10-14T13:36:32.000Z)
+         so when you want to save a date just send the result of new Date and mongoDB
+         save it automatically*/
+        var allocatedDate = new Date(parseDate).toISOString();
+        /*But when you want to filter result by date you have to pass a date format 2016-10-14T13:36:32.000Z and it
+        * converts the data to new Date("Fri, 14 Oct 2016 13:36:32 GMT") */
+        Park.find({"allocations.date": allocatedDate}, function (error, park) {
           if (!error) {
-            res.send(park);
+            if(park.length > 0) {
+              res.send(park);
+            }else {
+              res.status(404).send("The resource doesn't exist!");
+            }
+
           } else {
+            res.status(500).send("Server Error");
             console.log('ERROR : ', error);
           }
         });
