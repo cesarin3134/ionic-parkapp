8236b9b49b2fa81c104a2616adc4adebc09e1b43
Cesar Andavisa <cesar.andavisa@gmail.com>
2016-10-14 16:17:09 +0200

    added error handler 404

--- server/controllers/getParkByUserIdController.js
+++ server/controllers/getParkByUserIdController.js
@@ -2,13 +2,20 @@ module.exports = function (app, route) {
   var Park = app.models.park;
   return {
     "handler": function (req, res) {
-      Park.find({"allocations.employeeCode" : req.params.id}, function (error, park) {
-        if (!error) {
-          res.send(park);
-        } else {
-          console.log('ERROR : ', error);
-        }
-      })
+      if (req.params.id) {
+        var employeeCode = req.params.id.toUpperCase();
+        Park.find({"allocations.employeeCode": employeeCode}, function (error, park) {
+          if (!error) {
+            if(park.length > 0) {
+              res.send(park);
+            }else {
+              res.status(404).send("The resource doesn't exist!");
+            }
+          } else {
+            console.log('ERROR : ', error);
+          }
+        });
+      }
     },
     "method": "get"
   }
