981671d9598df7dd747b121dfbaa162fa1c749e9
Cesar Andavisa <cesar.andavisa@gmail.com>
2016-11-08 18:38:02 +0100

    Added config file for node server

--- server/app.js
+++ server/app.js
@@ -1,25 +1,42 @@
 var express = require('express');
+
 var mongoose = require('mongoose');
+
 var bodyParser = require('body-parser');
+
 var methodOverride = require('method-override');
+
+var colors = require('colors');
+
+var logger = require("mm-node-logger")(module);
+
+var pkg = require('./package.json');
+
+var config = require('./config/config');
+
 var _ = require('lodash');
-var localDb = "mongodb://localhost:27017/carparkapp";
-var prodDb = "mongodb://172.21.1.6:27017/carparkapp";
-var carParkDB = localDb;
+
 var app = express();
+
 var port = 5000;
 
 mongoose.set('debug', true);
 
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(bodyParser.json());
+
 app.use(methodOverride('X-HTTP-Method-Override'));
 
 //CORS support - it make your API public
 app.use(function (req, res, next) {
+
   res.header('Access-Control-Allow-Origin', '*');
+
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+
   res.header('Access-Control-Allow-Headers', 'Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With');
+
   if ('OPTIONS' === req.method) {
     res.status(204).send();
   }
@@ -29,20 +46,40 @@ app.use(function (req, res, next) {
 });
 
 //connect to DB
-mongoose.connect(carParkDB);
+mongoose.connect(config.mongodb.dev.dbURI);
 
 mongoose.connection.once('open', function () {
   //load models
   app.models = require('./models/index');
   //load routes
   var routes = require('./routes/routes');
+
   _.each(routes, function (controller, route) {
+
     var _controller = controller(app, route);
+
     app[_controller.method](route, _controller.handler);
+
   });
 
   var listener = app.listen(port, function () {
-    console.log('connected, listening on port ' + listener.address().port);
+
+    var serverBanner = ['',
+      '*************************************' + ' NODE SERVER '.yellow + '********************************************',
+      '*',
+      '* ' + pkg.description,
+      '* @version ' + pkg.version,
+      '* @author ' + pkg.author,
+      '* @copyright ' + new Date().getFullYear() + ' ' + pkg.author,
+      '*',
+      '*' + ' App started on port: ' .blue + config.server.port,
+      '*',
+      '*************************************************************************************************',
+      ''].join('\n');
+
+    logger.info(serverBanner);
+
+
   });
 
 });
---
+++ server/config/config.js
@@ -0,0 +1,33 @@
+/**
+ * An application configuration.
+ *
+ * @author    Cesar Andavisa
+ * @copyright Copyright (c) 2016, Cesar Andavisa
+ * @license    The MIT License {@link http://opensource.org/licenses/MIT}
+ */
+'use strict';
+
+var config = {};
+
+config.enviroment = 'development';
+
+config.server = {
+  host: '0.0.0.0',
+  port: 5000
+};
+
+// MongoDB settings
+config.mongodb = {
+  dev: {
+    dbURI: "mongodb://localhost:27017/carparkapp",
+    dbOptions: {"user": "", "pass": ""}
+  },
+  prod: {
+    dbURI: "mongodb://172.21.1.6:27017/carparkapp",
+    dbOptions: {"user": "", "pass": ""}
+  }
+
+};
+
+
+module.exports = config;
