8296dec9baf52dfe7da22e98cb992c1198d9f54e
Cesar Andavisa <cesar.andavisa@gmail.com>
2016-11-11 12:09:54 +0100

    nodejs config file optimized

--- server/config/config.js
+++ server/config/config.js
@@ -9,25 +9,45 @@
 
 var config = {};
 
-config.enviroment = 'development';
+config.environment = 'development';
 
-config.server = {
-  host: '172.16.2.55',
-  port: 3000
-};
+config.endpoint = [
+  {
+    host: '172.16.2.55',
+    port: 3000,
+    envName: "private ip"
+  },
+  {
+    host: '77.81.227.193',
+    port: 80,
+    envName: "production"
+  },
+  {
+    host: '127.0.0.1',
+    port: 3000,
+    envName: "local host"
+  }
+];
+
+config.mongodbEndpoint = [
+  {
+    dev: {
+      dbURI: "mongodb://localhost:27017/carparkapp",
+      dbOptions: {"user": "", "pass": ""}
+    }
+  },
+  {
+    prod: {
+      dbURI: "mongodb://172.21.1.6:27017/carparkapp",
+      dbOptions: {"user": "", "pass": ""}
+    }
+  }
+]
+
 
 // MongoDB settings
-config.mongodb = {
-  dev: {
-    dbURI: "mongodb://localhost:27017/carparkapp",
-    dbOptions: {"user": "", "pass": ""}
-  },
-  prod: {
-    dbURI: "mongodb://172.21.1.6:27017/carparkapp",
-    dbOptions: {"user": "", "pass": ""}
-  }
+config.mongodb = config.mongodbEndpoint[0];
 
-};
-
+config.server = config.endpoint[2];
 
 module.exports = config;
