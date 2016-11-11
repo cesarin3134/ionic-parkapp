9be38da1224dcc2bf86f47c6750a154a0c851f8a
Cesar Andavisa <cesar.andavisa@gmail.com>
2016-11-11 18:56:32 +0100

    global spinner for all request

--- app/main/main.js
+++ app/main/main.js
@@ -19,7 +19,7 @@ angular.module('main', [
   'LocalForageModule',
   'btford.socket-io',
   // TODO: load other modules selected during generation
-]).run(function ($ionicPlatform, $ionicLoading, $rootScope, $timeout) {
+]).run(function ($ionicPlatform, $ionicLoading, $rootScope) {
 
   $ionicPlatform.ready(function () {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -45,51 +45,64 @@ angular.module('main', [
     $ionicLoading.hide();
   });
 
-  $rootScope.$on('$stateChangeStart', function () {
-    console.log('loading');
-    $rootScope.$broadcast('loading:show');
-  });
+  /*  $rootScope.$on('$stateChangeStart', function () {
+   console.log('loading');
+   $rootScope.$broadcast('loading:show');
+   });*/
 
   $rootScope.$on('$stateChangeSuccess', function () {
-    console.log('done');
-    $timeout(function () {
-      $rootScope.$broadcast('loading:hide');
-    }, 1000);
+
+    $rootScope.$broadcast('loading:hide');
 
   });
-}).config(['$stateProvider', '$urlRouterProvider', '$localForageProvider', '$httpProvider', function ($stateProvider, $urlRouterProvider, $localForageProvider, $httpProvider) {
+}).config(['$stateProvider', '$urlRouterProvider', '$localForageProvider', '$httpProvider',
+  function ($stateProvider, $urlRouterProvider, $localForageProvider, $httpProvider) {
 
-  //initialize get if not there
-  if (!$httpProvider.defaults.headers.get) {
-    $httpProvider.defaults.headers.get = {};
-  }
+    //initialize get if not there
+    if (!$httpProvider.defaults.headers.get) {
+      $httpProvider.defaults.headers.get = {};
+    }
 
-  //disable IE ajax request caching
-  $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
-  // extra
-  $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
-  $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+    //disable IE ajax request caching
+    $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+    // extra
+    $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+    $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
 
-
-  $localForageProvider.config({
-    driver: 'localStorageWrapper',
-    name: 'parkCache'
-  });
-  // ROUTING with ui.router
-  $urlRouterProvider.otherwise('/home');
-  $stateProvider
-    // this state is placed in the <ion-nav-view> in the index.html
-    .state('home', {
-      url: '/home',
-      templateUrl: 'main/templates/home.view.html',
-      controller: 'homeCtrl as ctrl'
-    }).state('park-list', {
-      cache: false,
-      url: '/park-list',
-      params: {
-        'dataUser': null
-      },
-      templateUrl: 'main/templates/carParkList.view.html',
-      controller: 'carParkListCtrl as ctrl'
+    /*set spinner for all request*/
+    $httpProvider.interceptors.push(function ($rootScope) {
+      return {
+        request: function (config) {
+          $rootScope.$broadcast('loading:show');
+          return config;
+        },
+        response: function (response) {
+          $rootScope.$broadcast('loading:hide');
+          return response;
+        }
+      };
     });
-}]);
+
+
+    $localForageProvider.config({
+      driver: 'localStorageWrapper',
+      name: 'parkCache'
+    });
+    // ROUTING with ui.router
+    $urlRouterProvider.otherwise('/home');
+    $stateProvider
+      // this state is placed in the <ion-nav-view> in the index.html
+      .state('home', {
+        url: '/home',
+        templateUrl: 'main/templates/home.view.html',
+        controller: 'homeCtrl as ctrl'
+      }).state('park-list', {
+        cache: false,
+        url: '/park-list',
+        params: {
+          'dataUser': null
+        },
+        templateUrl: 'main/templates/carParkList.view.html',
+        controller: 'carParkListCtrl as ctrl'
+      });
+  }]);
